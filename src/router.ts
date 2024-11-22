a25034d8a066d639a7a74e7b85e7629f0e61fe30
Eduardo Cervantes Jaramillo <luis_edu_cj@hotmail.com>
2024-11-22 12:18:00 -0600

    Creando el Routing para Autenticación

--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,9 @@ import { Router } from "express";
 
 const router = Router()
 
-
+// Autenticación y registro
+router.post('/auth/register', (req, res) => {
+    res.json({ msg: "Petición POST" })
+});
 
 export default router
